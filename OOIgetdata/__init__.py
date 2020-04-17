8f993a64e82003525a38c67efaf1fd4bc918b050
OSU-Endyr <60718960+OSU-Endyr@users.noreply.github.com>
2020-04-17 14:21:34 -0500

    Update __init__.py

--- OOIgetdata/__init__.py
+++ OOIgetdata/__init__.py
@@ -1,91 +1,39 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "__init__.py",
-      "provenance": [],
-      "authorship_tag": "ABX9TyMUc899vQ9FaadxCaDTXoIb",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/OSU-Endyr/OOIgetdata/blob/master/OOIgetdata/__init__.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "metadata": {
-        "id": "-gQuhju_jQTt",
-        "colab_type": "code",
-        "colab": {}
-      },
-      "source": [
-        "import requests\n",
-        "import re\n",
-        "import xarray as xr\n",
-        "import os"
-      ],
-      "execution_count": 0,
-      "outputs": []
-    },
-    {
-      "cell_type": "code",
-      "metadata": {
-        "id": "86SYLYPQhXeF",
-        "colab_type": "code",
-        "colab": {}
-      },
-      "source": [
-        "def get_data(url,bad_inst=''):\n",
-        "  '''Function to grab all data from specified directory'''\n",
-        "  tds_url = 'https://opendap.oceanobservatories.org/thredds/dodsC'\n",
-        "  datasets = requests.get(url).text\n",
-        "  urls = re.findall(r'href=[\\'\"]?([^\\'\" >]+)', datasets)\n",
-        "  x = re.findall(r'(ooi/.*?.nc)', datasets)\n",
-        "  for i in x:\n",
-        "    if i.endswith('.nc') == False:\n",
-        "      x.remove(i)\n",
-        "  for i in x:\n",
-        "    try:\n",
-        "      float(i[-4])\n",
-        "    except:\n",
-        "      x.remove(i)\n",
-        "  datasets = [os.path.join(tds_url, i) for i in x]\n",
-        "  \n",
-        "  # Remove extraneous files if necessary\n",
-        "  selected_datasets = []\n",
-        "  for d in datasets:\n",
-        "    if (bad_inst) and bad_inst in d:\n",
-        "      pass\n",
-        "    elif 'ENG000' in d: #Remove engineering streams for gliders\n",
-        "      pass\n",
-        "    else:\n",
-        "      selected_datasets.append(d)\n",
-        "#   print(selected_datasets)\n",
-        "  \n",
-        "  # Load in dataset\n",
-        "  ds = xr.open_mfdataset(selected_datasets)\n",
-        "  ds = ds.swap_dims({'obs': 'time'}) # Swap the primary dimension\n",
-        "  # ds = ds.chunk({'time': 100}) # Used for optimization\n",
-        "  ds = ds.sortby('time') # Data from different deployments can overlap so we want to sort all data by time stamp.\n",
-        " \n",
-        "  return ds\n",
-        "\n"
-      ],
-      "execution_count": 0,
-      "outputs": []
-    }
-  ]
-}
+import requests
+import re
+import xarray as xr
+import os
+
+def get_data(url,bad_inst=''):
+  '''Function to grab all data from specified directory'''
+  tds_url = 'https://opendap.oceanobservatories.org/thredds/dodsC'
+  datasets = requests.get(url).text
+  urls = re.findall(r'href=[\'"]?([^\'" >]+)', datasets)
+  x = re.findall(r'(ooi/.*?.nc)', datasets)
+  for i in x:
+    if i.endswith('.nc') == False:
+      x.remove(i)
+  for i in x:
+    try:
+      float(i[-4])
+    except:
+      x.remove(i)
+  datasets = [os.path.join(tds_url, i) for i in x]
+  
+  # Remove extraneous files if necessary
+  selected_datasets = []
+  for d in datasets:
+    if (bad_inst) and bad_inst in d:
+      pass
+    elif 'ENG000' in d: #Remove engineering streams for gliders
+      pass
+    else:
+      selected_datasets.append(d)
+#   print(selected_datasets)
+  
+  # Load in dataset
+  ds = xr.open_mfdataset(selected_datasets)
+  ds = ds.swap_dims({'obs': 'time'}) # Swap the primary dimension
+  # ds = ds.chunk({'time': 100}) # Used for optimization
+  ds = ds.sortby('time') # Data from different deployments can overlap so we want to sort all data by time stamp.
+ 
+  return ds
